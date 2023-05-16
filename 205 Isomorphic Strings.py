6a4f7ecf1c981da8618ba9262207892f23c4d081
LocTL7451 <Loc.Lien7451@gmail.com>
2023-05-16 22:17:08 +1000

    Completed problem 205

---
+++ 205 Isomorphic Strings.py
@@ -0,0 +1,53 @@
+# 205. Isomorphic Strings
+# https://leetcode.com/problems/isomorphic-strings/?envType=study-plan-v2&id=top-interview-150
+
+"""
+Runtime | 71 ms
+Beats | 8.45%
+Memory | 17.1 MB
+Beats | 5.42%
+"""
+
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        mappedSPositions = []
+        mappedTPositions = []
+        retFlag = False
+        for i in s:
+            mappedSPositions.append(s.index(i))
+        for j in t:
+            mappedTPositions.append(t.index(j))
+        print("T Array: {} \nS Array: {}".format(mappedTPositions,mappedSPositions))
+        if mappedSPositions == mappedTPositions:
+            retFlag = True  
+        return retFlag
+        """
+            mappedDictS = {}
+            mappedDictT = {}
+            matchFlag = False
+            for i in range(len(s)):
+                if s[i] == t[i]:
+                    if s[i] in mappedDictS:
+                        matchFlag = True
+                    else:
+                        mappedDictS[s[i]] = t[i]
+                        matchFlag = True
+                else:
+                    if s[i] in mappedDictS:
+                        if mappedDictS[s[i]] == t[i]:
+                            matchFlag = True
+                        else:
+                            matchFlag = False
+                            break
+                    else:
+                        mappedDictS[s[i]] = t[i]
+                        matchFlag = True
+                        
+            return matchFlag
+        """
+sol = Solution()
+print(sol.isIsomorphic("babq", "babc"))
+                
+ 
+        
+    
