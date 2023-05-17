67d95228ffcb1e186b280a4e1410d539af603a01
LocTL7451 <Loc.Lien7451@gmail.com>
2023-05-17 15:34:49 +1000

    Completed problem 66

--- 66 Plus One.py
+++ 66 Plus One.py
@@ -2,31 +2,26 @@
 # https://leetcode.com/problems/plus-one/?envType=study-plan-v2&id=top-interview-150
 
 """
-Runtime | 50 ms
-Beats |14.87%
+Runtime | 53 ms
+Beats | 41.72%
 Memory | 16.3 MB
-Beats | 18.82%
+Beats | 8.52%
 """
 
 """
-    I attempted a stupid approach using stacks and popping based on whether the index was in the first or second half
-    but realised the problem is so stupidly easy and achievable using a reversed version of the string of the number
-    to check if the number string is a palindrome.
+    I realised after failing to complete the problem using string and array casting upon the integer n due to TLE and out of memory for 
+    string type conversions, that the easiest way is to break down the way factorial 0's are created.
+    For every time n can be divided by 5, we add a 0. Simple as that.
+    So I created a recursive loop for continue looping the function until it hits the base case where the current n is no longer able
+    to be divided by 5
 """
 
 
-
+import math
 class Solution:
-    def plusOne(self, digits):
-        strDigits = ""
-        retArr = []
-        for i in digits:
-            strDigits += str(i)
-        intDigits = int(strDigits)+1
-        for i in str(intDigits):
-            retArr.append(int(i))
-        return retArr
-
+    def trailingZeroes(self, n: int) -> int:
+        if n < 5: return 0
+        else: return (int(n/5) + self.trailingZeroes(int(n/5)))
 
 sol = Solution()
-print(sol.plusOne([4,3,2,1]))
+print(sol.trailingZeroes(5))
