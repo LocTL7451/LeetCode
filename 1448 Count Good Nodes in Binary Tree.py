2edf94cdeafea1c7011b107b4774f24fc00558de
Loc Lien <Loc.Lien7451@gmail.com>
2023-06-02 20:18:38 +1000

    Completed problem 1448

--- 1448 Count Good Nodes in Binary Tree.py
+++ 1448 Count Good Nodes in Binary Tree.py
@@ -2,14 +2,21 @@
 # https://leetcode.com/problems/count-good-nodes-in-binary-tree/?envType=study-plan-v2&envId=leetcode-75
 
 """ 
-Runtime | 45 ms
-Beats | 43.4%
-Memory | 16.3 MB
-Beats | 38.69%
+Runtime | 309 ms
+Beats | 5.30%
+Memory | 35.4 MB
+Beats | 18.6%
 """
 
 """ 
-    The approach here is divide n by 5 until you can't, then by 3 then by 2, resulting in 1 or not 1 if n has another prime factor. 
+    The approach to solve this solution is to utilise a common DFS pattern in coding questions. These questions usually entail 
+    traversing through the tree and keeping track of some arbitrary value, in this case being the current max val of the traversed nodes
+    so far leading up to the current node. The way we do this is through using DFS, as DFS allows us to traverse to from the root
+    to all leaf nodes. This is facilitated through the usage of the Queue data structure, allowing us to walk down each path node by node 
+    by appending to the end of the queue and popping from the front (position 0) of the queue. 
+    For each node, we check if the node's value is greater than the max val of the current traversed path, incrementing the return 
+    value if it is. We then continue traversing the tree by adding the left and right nodes of the current node if they exist, adding
+    the extra parameter of the max val which will only change if the current node's value is greater than the current cumulative max.
 """
 # Definition for a binary tree node.
 # class TreeNode:
@@ -24,8 +31,15 @@ class Solution:
         ret = 0
         dfsQueue.append((root,-m.inf))
         
-        
-        
+        while dfsQueue:
+            currNode = dfsQueue.pop(0)
+            if currNode[0].val > currNode[1]:
+                ret+=1
+            if currNode[0].left:
+                dfsQueue.append((currNode[0].left, max(currNode[0].val, currNode[1])))
+            if currNode[0].right:
+                dfsQueue.append((currNode[0].right, max(currNode[0].val, currNode[1])))
+        return ret
         
 sol = Solution()
 print(sol.isUgly(14))
